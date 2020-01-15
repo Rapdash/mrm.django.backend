cdfdd5ea03d1306784769cb8ef6f5d78ae2709fb
Frederick Johnson <fritzj96@gmail.com>
2020-01-14 23:45:02 -0800

    added: Knox as default auth class

--- core/settings.py
+++ core/settings.py
@@ -41,7 +41,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'knox'
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': ('knox.auth.TokenAuthentication',)
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
