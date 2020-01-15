abf0c7e7a4235ec879d685daed86ed6315b42300
Frederick Johnson <fritzj96@gmail.com>
2020-01-15 09:35:28 -0800

    added: Account model

--- branches/models.py
+++ branches/models.py
@@ -1,6 +1,13 @@
-from django.db.models import Model, CharField
+from django.db.models import Model, CharField, ForeignKey, CASCADE
 
 # Create your models here.
 class Branch(Model):
   address = CharField(max_length=255)
   name = CharField(max_length=255)
+
+ACCOUNT_TYPE_CHOICES = (('CHK', 'Checking'), ('SVG', 'Savings'), ('CRE', 'Credit'))
+
+class Account(Model):
+  branch = ForeignKey(Branch, CASCADE, related_name="account")
+  owner_name = CharField(max_length=255)
+  account_type = CharField(max_length=255, choices=ACCOUNT_TYPE_CHOICES)
