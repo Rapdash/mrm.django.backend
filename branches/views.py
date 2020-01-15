412e3f8637606b0fed2a7a243f0d7e5f7b2c993f
Frederick Johnson <fritzj96@gmail.com>
2020-01-15 12:30:29 -0800

    added: BranchSerializer

--- branches/views.py
+++ branches/views.py
@@ -1,9 +1,13 @@
 from .models import Branch, Account
+from .serializers import 
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.generics import ListAPIView, CreateAPIView
 
 class ListBranchesView(ListAPIView):
   queryset = Branch.objects.all()
+  serializer_class = BranchSerializer
   permission_classes = (IsAuthenticated,)
 
 class CreateBranchesView(CreateAPIView):
+  queryset = Branch.objects.all()
+  s
