f168034995e94259d10bdcc348dc0e50d7e72b34
Frederick Johnson <fritzj96@gmail.com>
2020-01-15 09:29:46 -0800

    added: Branch Model

--- branches/models.py
+++ branches/models.py
@@ -1,3 +1,6 @@
-from django.db import models
+from django.db.models import Model, CharField
 
 # Create your models here.
+class Branch(Model):
+  address = CharField(max_length=255)
+  name = CharField(max_length=255)
