6ca0eaef0192ce92d2ea6c6194445bffe2115b2f
Frederick Johnson <fritzj96@gmail.com>
2020-01-15 00:00:17 -0800

    removed: auth app

--- core/settings.py
+++ core/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'knox',
-    'auth'
 ]
 
 REST_FRAMEWORK = {
--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from knox import views as knox_views
+from .views import LoginView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('auth.urls')),
+    path('auth/login/', LoginView.as_view(), name='knox_login'),
+     path('logout/', knox_views.LogoutView.as_view(), name='knox_logout'),
+     path('logoutall/', knox_views.LogoutAllView.as_view(), name='knox_logoutall'),
+
 ]
