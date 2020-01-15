a399910518a822db3b2933c46b327956679428ed
Frederick Johnson <fritzj96@gmail.com>
2020-01-15 10:04:11 -0800

    added: ListBranchesView

--- branches/views.py
+++ branches/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from .models import Branch, Account
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.generics import ListAPIView, CreateAPIView
 
-# Create your views here.
+class ListBranchesView(ListAPIView):
+  queryset = Branch.objects.all()
+  permission_classes = (IsAuthenticated,)
+
+class CreateBranchesView(CreateAPIView):
